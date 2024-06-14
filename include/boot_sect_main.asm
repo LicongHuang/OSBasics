bff6da7478b25e426c98634338b4e43c41aef3c4
Huang Licong <alexliconghuang@gmail.com>
2024-06-14 12:00:50 +0800

    Update boot_sect_main.asm

--- include/boot_sect_main.asm
+++ include/boot_sect_main.asm
@@ -2,26 +2,29 @@
 
 mov bx, HELLO
 call print
-
 call print_nl
 
 mov bx, GOODBYE
 call print
-
 call print_nl
 
+mov dx, 0x1337  ; Value to print in hex
+call print_hex
+mov dx, 0xc0de
+call print_hex
+call print_nl
 jmp $
 
-
-HELLO: 
+HELLO:
   db "Hello, world!", 0
 
-GOODBYE: 
+GOODBYE:
   db "Goodbye, world!", 0
 
+HEX_OUT:
+  db "0x0000", 0  ; Buffer for the hex string
 
 %include "boot_sect_print.asm"
 
 times 510-($-$$) db 0
 dw 0xaa55
-
